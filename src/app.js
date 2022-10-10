9f65123afdd5af8f57692cd781e5fcece6277ddd
joan <joanflores905@gmail.com>
2022-10-10 14:11:46 +0000

    version 2

--- src/app.js
+++ src/app.js
@@ -5,7 +5,32 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function excuseGenerator() {
+  let whoOptions = ["the dog", "my granma", "his turtle", "my bird"];
+  let didOptions = ["eat", "pissed", "crushed", "broked"];
+  let objectOptions = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let firstPartAnswer = "Sorry..!";
+  let whoAnswer =
+    whoOptions[Math.round(Math.random() * (whoOptions.length - 1))];
+  let didAnswer =
+    didOptions[Math.round(Math.random() * (didOptions.length - 1))];
+  let objectAnswer =
+    objectOptions[Math.round(Math.random() * (objectOptions.length - 1))];
+
+  document.querySelector("#answerDocument").innerHTML = firstPartAnswer.concat(
+    " ",
+    whoAnswer,
+    " ",
+    didAnswer,
+    " ",
+    objectAnswer,
+    " "
+  );
 };
